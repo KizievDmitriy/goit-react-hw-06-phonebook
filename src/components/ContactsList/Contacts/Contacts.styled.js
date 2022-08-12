2f8abe212945bc0f9c956211c112c0e5818a45d1
KizievDmitriy <kizievdmitrii@gmail.com>
2022-08-12 15:24:01 +0300

    add normalize

--- src/components/ContactsList/Contacts/Contacts.styled.js
+++ src/components/ContactsList/Contacts/Contacts.styled.js
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 
 export const ItemContact = styled.li`
-  padding: 16px;
+  padding: 16px 0;
   font-size: 24px;
   align-items: center;
   display: flex;
 
-  &:before {
+  /* &:before {
     content: '';
     font-size: 10px;
     margin-left: -14px;
     padding-right: 8px;
     vertical-align: 20%;
-  }
+  } */
 `;
 
 export const DeleteBtn = styled.button`
@@ -34,6 +34,6 @@ export const Name = styled.p`
   text-transform: uppercase;
 `;
 export const Number = styled.p`
-margin-left: auto;
+  margin-left: auto;
   margin-right: 20px;
 `;
